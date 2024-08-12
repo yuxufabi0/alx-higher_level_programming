69dcfa02e0845c2256f0f1899fa2de13b06ffda8
Yuxufabio <yuxufabio14@yahoo.com>
2024-08-12 11:49:06 +0100

    Using command-line argument

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const process = require('process');
+if (process.argv.length < 3) console.log('No argument');
+else if (process.argv.length === 3) console.log('Argument found');
+else console.log('Arguments found');
