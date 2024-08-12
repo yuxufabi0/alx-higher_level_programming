27f59db34e214f445acae9ec8d75e65b858ffc94
Yuxufabio <yuxufabio14@yahoo.com>
2024-08-12 14:29:49 +0100

    Computing factorialof a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const process = require('process');
+const number = parseInt(process.argv[2]);
+
+if (isNaN(number)) console.log(1);
+else factorial(number);
+
+function factorial (a) {
+  let temp = a;
+  while (a > 1) {
+    temp = temp * (a - 1);
+    a--;
+  }
+  console.log(temp);
+}
