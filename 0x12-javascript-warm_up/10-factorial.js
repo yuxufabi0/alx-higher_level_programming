0bc0a99dc636e4a6f7ee858f5d81b7a4ae0448b8
Yuxufabio <yuxufabio14@yahoo.com>
2024-08-13 23:37:10 +0100

    fixing output

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,8 @@
 const process = require('process');
 const number = parseInt(process.argv[2]);
 
-if (isNaN(number) || number < 1) console.log(1);
+if (number < 0) console.log(-1);
+else if (isNaN(number) || number === 0) console.log(1);
 else factorial(number);
 
 function factorial (a) {
